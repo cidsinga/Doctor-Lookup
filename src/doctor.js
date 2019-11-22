7333fd007f923beeba1570f19f4e5a34db30c6d9
cidsinga <56556816+cidsinga@users.noreply.github.com>
2019-11-22 09:20:47 -0800

    add initial front and back end js

--- src/doctor.js
+++ src/doctor.js
@@ -1,1 +1,11 @@
-// export function Object
+export function DoctorList{
+  async getDocByCondish(condish) {
+    try {
+      let response = await fetch...
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+
+    }
+  }
+}
