f61c3685463f76be634c22192eadcdea8ee38432
cidsinga <56556816+cidsinga@users.noreply.github.com>
2019-11-22 09:58:45 -0800

    add corrections to doctor.js and added click function in main.js

--- src/doctor.js
+++ src/doctor.js
@@ -1,7 +1,7 @@
-export function DoctorList{
+export class DoctorList{
   async getDocByCondish(condish) {
     try {
-      let response = await fetch...
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?skip=0&limit=10&user_key=${process.env.API_KEY}&location=or-portland&query=${condish}`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { Object } from './doctor.js';
+import { DoctorList } from './doctor.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function() {
+  $('#button').click(function(event){
+    event.preventDefault;
+    const condish = $('#medCondition').val();
+    $('#medCondition').val('');
+    (async () => {
+      let doctorList = new DoctorList();
+      const response = await doctorList.getDocByCondish(condish); // replace doc
+      getElements(response);
+    })();
+    
+  })
+})
